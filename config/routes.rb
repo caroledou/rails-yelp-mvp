9ba6d0d36d5774b5368d5b85ca99c60817adc557
Carole Douchy <carole.douchy@ehl.ch>
2019-02-13 14:16:39 +0100

    seeds, models and controllers done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'restaurants', to: 'restaurants#index'
+  get 'restaurants/new', to: 'restaurants#new'
+  post 'restaurants', to: 'restaurants#create'
+  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :review_new
+  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create'
+  get 'restaurants/:restaurant_id/reviews/', to: 'reviews#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Le Pointu',
+    address: "Riponne, Lausanne",
+    phone_number: "021 263 19 28",
+    category: "french"
+  },
+  {
+    name: 'Five Guys',
+    address: "Gare, Genève",
+    phone_number: "022 678 29 86",
+    category: "french"
+  },
+  {
+    name: 'Luigia',
+    address: "Terrassière, Genève",
+    phone_number: "022 243 98 23",
+    category: "italian"
+
+  },
+  {
+    name: 'Sushi Shop',
+    address: "Coulouvrenière, Genève",
+    phone_number: "022 363 59 28",
+    category: "japanese"
+  },
+  {
+    name: 'Bao kitchen',
+    address: "Plainpalais, Genève",
+    phone_number: "022 213 49 78",
+    category: "chinese"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
